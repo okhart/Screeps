0077e46840386f19dc55061f88c14ae9a8146d3d
Artem V. Okhotnikov <okhotnikov.artem@gmail.com>
2020-01-18 17:48:01 +0300

    Soldiers can go to FlagAttack and then, well, attack :)

--- role.soldier.js
+++ role.soldier.js
@@ -1,10 +1,19 @@
+//Soldiers are basic war units with weak hits and melee attack
+
 var roleSoldier = {
     run: function(creep)
     {
-        var target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
-        if(creep.rangedAttack(target) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(target, {visualizePathStyle: {stroke: '#ff0000'}});
+        if(Game.flags.FlagAttack && creep.memory.hasReachedFlag == false)
+        {
+            creep.moveTo(Game.flags.FlagAttack);
+            if (creep.pos.getRangeTo(Game.flags.FlagAttack) < 5)
+                creep.emory.hasReachedFlag == true;
         }
+        var target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
+        if(!target)
+            target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, s => s.type != STRUCTURE_CONTROLLER);
+        if(creep.attack(target) == ERR_NOT_IN_RANGE) 
+            creep.moveTo(target, {visualizePathStyle: {stroke: '#ff0000'}});
     }
 };
 
